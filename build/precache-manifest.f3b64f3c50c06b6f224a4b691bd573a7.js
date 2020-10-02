d135a77f52535b65d9f218dbde83f64856755b2e
madhu charlie <madhucharlie01@gmail.com>
2020-10-02 22:03:43 +0530

    myntra

---
+++ build/precache-manifest.f3b64f3c50c06b6f224a4b691bd573a7.js
@@ -0,0 +1,318 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1597c83d317e73435f008f7fdf6e25e1",
+    "url": "/myntra/index.html"
+  },
+  {
+    "revision": "fd306dacc661035cd6b0",
+    "url": "/myntra/static/css/2.062a711b.chunk.css"
+  },
+  {
+    "revision": "9ad68ef6bc799b43eab8",
+    "url": "/myntra/static/css/main.11b9998b.chunk.css"
+  },
+  {
+    "revision": "fd306dacc661035cd6b0",
+    "url": "/myntra/static/js/2.6e806aae.chunk.js"
+  },
+  {
+    "revision": "de5933bbfe8f970953aa351dcd16a9a6",
+    "url": "/myntra/static/js/2.6e806aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad68ef6bc799b43eab8",
+    "url": "/myntra/static/js/main.89335df4.chunk.js"
+  },
+  {
+    "revision": "4aa1c4831a30555e65b7",
+    "url": "/myntra/static/js/runtime-main.b84cc036.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/myntra/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/myntra/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/myntra/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/myntra/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/myntra/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/myntra/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/myntra/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/myntra/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/myntra/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/myntra/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/myntra/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/myntra/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/myntra/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/myntra/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/myntra/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/myntra/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/myntra/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "4195066691c01357fdd1f4e46c461a54",
+    "url": "/myntra/static/media/home-1.41950666.png"
+  },
+  {
+    "revision": "013f28a855211c2effef8edd00d74314",
+    "url": "/myntra/static/media/home-11.013f28a8.jpg"
+  },
+  {
+    "revision": "5c51016e7cba5e53d6fc9145dd83af30",
+    "url": "/myntra/static/media/home-12.5c51016e.jpg"
+  },
+  {
+    "revision": "45bf081222de4f8a5aa7915f8bc1df70",
+    "url": "/myntra/static/media/home-13.45bf0812.jpg"
+  },
+  {
+    "revision": "ea90d53a1ded89829317ecc4c606ea3b",
+    "url": "/myntra/static/media/home-14.ea90d53a.jpg"
+  },
+  {
+    "revision": "5bb2746a164180efba629fd1f62e92a8",
+    "url": "/myntra/static/media/home-15.5bb2746a.jpg"
+  },
+  {
+    "revision": "1720e082a814f2630275d0e0b00a1182",
+    "url": "/myntra/static/media/home-2.1720e082.jpg"
+  },
+  {
+    "revision": "4d5b1e18c048c9accdf1a82f8bbfba4e",
+    "url": "/myntra/static/media/home-21.4d5b1e18.jpg"
+  },
+  {
+    "revision": "a82fee1bce4feff5d54608ef922a0bfc",
+    "url": "/myntra/static/media/home-22.a82fee1b.jpg"
+  },
+  {
+    "revision": "df5dfc44f2b3e62c4f81bfe78924f5f6",
+    "url": "/myntra/static/media/home-23.df5dfc44.jpg"
+  },
+  {
+    "revision": "9d8e58a06e7bc4c797aa007c82c0e833",
+    "url": "/myntra/static/media/home-24.9d8e58a0.jpg"
+  },
+  {
+    "revision": "ea325c368c99d3f583494ec03f4f615d",
+    "url": "/myntra/static/media/home-25.ea325c36.jpg"
+  },
+  {
+    "revision": "1b70ead6e2c186e6eb66a12af5e23ced",
+    "url": "/myntra/static/media/home-26.1b70ead6.jpg"
+  },
+  {
+    "revision": "0c7c72c4d002b7b53308021a0d16a6af",
+    "url": "/myntra/static/media/home-27.0c7c72c4.jpg"
+  },
+  {
+    "revision": "515a00b1fdeab343d05840cd6214b5e7",
+    "url": "/myntra/static/media/home-28.515a00b1.jpg"
+  },
+  {
+    "revision": "4ef73dcf27b425d12c8ec20fea087301",
+    "url": "/myntra/static/media/home-29.4ef73dcf.jpg"
+  },
+  {
+    "revision": "2b1ff1c3a6363ada41ab0658d7cb0672",
+    "url": "/myntra/static/media/home-3.2b1ff1c3.jpg"
+  },
+  {
+    "revision": "01ae0256a0033f1f92c86a64d073b9ca",
+    "url": "/myntra/static/media/home-30.01ae0256.jpg"
+  },
+  {
+    "revision": "7ab9cb4ef143af5c551b6db06b60a590",
+    "url": "/myntra/static/media/home-31.7ab9cb4e.jpg"
+  },
+  {
+    "revision": "965e7fd4873987796d35228f57a83d0b",
+    "url": "/myntra/static/media/home-32.965e7fd4.jpg"
+  },
+  {
+    "revision": "0b537cc1ae9ff3eda0f63fe9a20e20e0",
+    "url": "/myntra/static/media/home-33.0b537cc1.jpg"
+  },
+  {
+    "revision": "505f81256411f25f37b31287c65b3cf5",
+    "url": "/myntra/static/media/home-34.505f8125.jpg"
+  },
+  {
+    "revision": "4dd9482fb23caf33c865394014c7a6ac",
+    "url": "/myntra/static/media/home-35.4dd9482f.jpg"
+  },
+  {
+    "revision": "a9c78026c2b4a08d49129912ba5590e1",
+    "url": "/myntra/static/media/home-36.a9c78026.jpg"
+  },
+  {
+    "revision": "6d12bce4f501a50b07aa001f4c361988",
+    "url": "/myntra/static/media/home-37.6d12bce4.jpg"
+  },
+  {
+    "revision": "c6e3ed2b57ab0ab79dbcc999f91cc524",
+    "url": "/myntra/static/media/home-38.c6e3ed2b.jpg"
+  },
+  {
+    "revision": "dd45cf13459957570a763864f4bf5e66",
+    "url": "/myntra/static/media/home-39.dd45cf13.jpg"
+  },
+  {
+    "revision": "174556237d38b4b5bdd827360787332c",
+    "url": "/myntra/static/media/home-4.17455623.jpg"
+  },
+  {
+    "revision": "1961d63d5be9f57381d43130b1281b8f",
+    "url": "/myntra/static/media/home-40-1.1961d63d.jpg"
+  },
+  {
+    "revision": "43deb2a7c8ebc90d1ebb5ed38059e8da",
+    "url": "/myntra/static/media/home-40-2.43deb2a7.jpg"
+  },
+  {
+    "revision": "b753cecc8e1ad647741093bbf7e8942f",
+    "url": "/myntra/static/media/home-40.b753cecc.jpg"
+  },
+  {
+    "revision": "ed20f106f9a9f768848412082937a158",
+    "url": "/myntra/static/media/home-41.ed20f106.jpg"
+  },
+  {
+    "revision": "89884e9b8c2bb168ffc7984557413b3c",
+    "url": "/myntra/static/media/home-42.89884e9b.jpg"
+  },
+  {
+    "revision": "2c708d026cef3240ef0c4d6745c10361",
+    "url": "/myntra/static/media/home-43.2c708d02.jpg"
+  },
+  {
+    "revision": "c88bd3c6a5c146f4f1004492aea5a32b",
+    "url": "/myntra/static/media/home-44.c88bd3c6.jpg"
+  },
+  {
+    "revision": "aa1aef5a3efb044646d76c7a1c311ca4",
+    "url": "/myntra/static/media/home-45.aa1aef5a.jpg"
+  },
+  {
+    "revision": "44735b4880f05ed155ecb0bc295a2c2f",
+    "url": "/myntra/static/media/home-46.44735b48.jpg"
+  },
+  {
+    "revision": "ce37a109fba80a76b5300f2096dc3946",
+    "url": "/myntra/static/media/home-47.ce37a109.jpg"
+  },
+  {
+    "revision": "d3a9781571a88c4f98c0155b1dd4ec26",
+    "url": "/myntra/static/media/home-48.d3a97815.jpg"
+  },
+  {
+    "revision": "a4fcdad2c79a2305c8ac2057515b2a86",
+    "url": "/myntra/static/media/home-49.a4fcdad2.jpg"
+  },
+  {
+    "revision": "9a3623902f086d02dd7551a5f2700179",
+    "url": "/myntra/static/media/home-5.9a362390.jpg"
+  },
+  {
+    "revision": "b17caca441920ab6ccf1716ec4c4b18e",
+    "url": "/myntra/static/media/home-50.b17caca4.jpg"
+  },
+  {
+    "revision": "050ac6a1b9009a31420ebc189f5e1fba",
+    "url": "/myntra/static/media/home-51.050ac6a1.jpg"
+  },
+  {
+    "revision": "03bb3184a241b165ab832061f49ba01d",
+    "url": "/myntra/static/media/home-52.03bb3184.jpg"
+  },
+  {
+    "revision": "d705311b133884caae99fc16c4d7abd7",
+    "url": "/myntra/static/media/home-53.d705311b.jpg"
+  },
+  {
+    "revision": "bc9a811fba82d6b94238b1bdbc034849",
+    "url": "/myntra/static/media/home-54.bc9a811f.jpg"
+  },
+  {
+    "revision": "d35f3b53503b8efafc48a7d909d0cb39",
+    "url": "/myntra/static/media/home-55.d35f3b53.jpg"
+  },
+  {
+    "revision": "0d92e7b3c7f1942727878499fa19cbad",
+    "url": "/myntra/static/media/home-6.0d92e7b3.jpg"
+  },
+  {
+    "revision": "c8190cfa7f105a6018d600341c057873",
+    "url": "/myntra/static/media/home-61.c8190cfa.jpg"
+  },
+  {
+    "revision": "b0f79e19b18bba4624916cb71e8fadc9",
+    "url": "/myntra/static/media/home-62.b0f79e19.jpg"
+  },
+  {
+    "revision": "f9816ae86685c27270d80055e0f5892d",
+    "url": "/myntra/static/media/home-63.f9816ae8.jpg"
+  },
+  {
+    "revision": "3a3066459114f0622381e53a3cbc4dca",
+    "url": "/myntra/static/media/home-64.3a306645.jpg"
+  },
+  {
+    "revision": "ce9fb9b0f38a4e2f5c72a4a183fe1504",
+    "url": "/myntra/static/media/home-65.ce9fb9b0.jpg"
+  },
+  {
+    "revision": "8a0ef323a09a67d9d9728e80690e0fb4",
+    "url": "/myntra/static/media/home-7.8a0ef323.jpg"
+  },
+  {
+    "revision": "0ae8894a2f98e633f287337bb8818f9c",
+    "url": "/myntra/static/media/myntralogo.0ae8894a.png"
+  }
+]);
